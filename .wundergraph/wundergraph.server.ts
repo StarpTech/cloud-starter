77a98bb94d18862d069dc531120f8e606a9e3fea
StarpTech <deusdustin@gmail.com>
2023-06-18 17:35:38 +0200

    test

--- .wundergraph/wundergraph.server.ts
+++ .wundergraph/wundergraph.server.ts
@@ -8,8 +8,6 @@ export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
       Dragons: {
         postResolve: async (hook) => {
           hook.log.info("postResolve hook for Dragons");
-          console.log("test 11 " + process.env.WG_OTEL_AUTH_TOKEN)
-          console.log("foo 11 " + process.env.FOO)
         },
       },
     },
